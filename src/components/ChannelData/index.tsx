c0893e6cb50bc76c10466afd2d85cd3937821275
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-01-05 09:53:58 -0300

    finished channel data component

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import {Container} from './styles'
+import {Container,Messages,Input,InputIcon,InputWrapper} from './styles'
 
 const ChannelData: React.FC = () => {
     return (
         <Container>
-
+            <Messages/>
+            <InputWrapper>
+                <Input/>
+                <InputIcon/>
+            </InputWrapper>
         </Container>
 
     )
