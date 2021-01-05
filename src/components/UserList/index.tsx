96a41d37dde897f8f384ab72b2f14097d8d28b05
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-01-05 09:36:13 -0300

    finished userList Component

--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -1,10 +1,53 @@
 import React from 'react'
-import {Container} from './styles'
+import {Container,Role,User,Avatar} from './styles'
+interface UserProps {
+    nickname:string;
+}
+
+const UserRow: React.FC<UserProps> = ({nickname}) =>{
+    return(
+        <User>
+            <Avatar />
+            <strong>{nickname}</strong>
+        </User>
+    )
+}
+
 
 const UserList: React.FC = () => {
     return (
-        <Container>
 
+        <Container>
+            <Role>Online - 1</Role>
+            <UserRow nickname="Lucas Pires"/>
+            <Role>Offline - 10</Role>
+            <UserRow nickname="Diego Martins"/>
+            <UserRow nickname="João Ninguém"/>  
+            <UserRow nickname="João Ninguém"/>     
+            <UserRow nickname="João Ninguém"/>  
+            <UserRow nickname="João Ninguém"/>        
+            <UserRow nickname="João Ninguém"/>  
+            <UserRow nickname="João Ninguém"/>     
+            <UserRow nickname="João Ninguém"/>  
+            <UserRow nickname="João Ninguém"/>
+            <UserRow nickname="Diego Martins"/>
+            <UserRow nickname="João Ninguém"/>  
+            <UserRow nickname="João Ninguém"/>     
+            <UserRow nickname="João Ninguém"/>  
+            <UserRow nickname="João Ninguém"/>        
+            <UserRow nickname="João Ninguém"/>  
+            <UserRow nickname="João Ninguém"/>     
+            <UserRow nickname="João Ninguém"/>  
+            <UserRow nickname="João Ninguém"/>
+            <UserRow nickname="Diego Martins"/>
+            <UserRow nickname="João Ninguém"/>  
+            <UserRow nickname="João Ninguém"/>     
+            <UserRow nickname="João Ninguém"/>  
+            <UserRow nickname="João Ninguém"/>        
+            <UserRow nickname="João Ninguém"/>  
+            <UserRow nickname="João Ninguém"/>     
+            <UserRow nickname="João Ninguém"/>  
+            <UserRow nickname="João Ninguém"/>      
         </Container>
 
     )
--- src/components/UserList/styles.ts
+++ src/components/UserList/styles.ts
@@ -1,5 +1,61 @@
 import styled from "styled-components";
 export const Container = styled.div`
+    grid-area:UL;
+    display:flex;
+    flex-direction:column;
 
+    padding:3px 6px 0 16px;
+    background-color:var(--secondary);
+    max-height:calc(100vh - 46px);
+    overflow-y:scroll;
+    ::-webkit-scrollbar{
+        width:4px;
+        }
+    ::-webkit-scrollbar-thumb{
+        background-color:var(--tertiary);
+        border-radius:4px;
+        }
+
+    ::-webkit-scrollbar-track{
+        background-color:var(--secondary);
+        }
 `;
 
+export const Role = styled.span`
+margin-top:20px;
+text-transform:uppercase;
+font-size:12px;
+font-weight:500;
+color:var(--gray)
+`;
+
+export const Avatar = styled.div`
+width: 32px;
+height: 32px;
+border-radius: 50%;
+background-color: var(--gray);
+`;
+
+export const User = styled.div`
+    margin-top:5px;
+    padding:5px;
+    display:flex;
+    align-items:center;
+    border-radius:4px;
+    background:transparent;
+    transition:background .2s;
+    &:hover{
+        background:rgba(255,255,255,0.1);
+    }
+
+    >strong{
+        margin-left:13px;
+        color:#Fff;
+        font-weight:500;
+        opacity:0.7;
+        white-space:nowrap;
+        text-overflow:ellipsis;
+        overflow:hidden;
+    }
+    
+`;
